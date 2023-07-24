bfbdccd921818d6077f128954da75e8e1745a880
VIBINK <vibin1023@gmail.com>
2023-07-24 18:37:28 +0530

    first_commit

---
+++ lib/widgets/menu_items.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MenuItems extends StatelessWidget {
+  final String svglcon, itemName;
+  const MenuItems({
+    Key? key,
+    required this.svglcon,
+    required this.itemName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(svglcon),
+      title: Text(itemName),
+    );
+  }
+}
